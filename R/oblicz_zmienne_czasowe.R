51bb69923bcb48d7ee2b1bdd8ba286bbd98061c7
Mateusz Żółtak <zozlak@zozlak.org>
2016-01-17 18:25:41 +0100

    Poprawki w wyliczaniu zmiennych czasowych

--- R/oblicz_zmienne_czasowe.R
+++ R/oblicz_zmienne_czasowe.R
@@ -7,15 +7,15 @@
 #' @import dplyr
 oblicz_zmienne_czasowe = function(dane, utrataEtatu, multidplyr = TRUE){
   stopifnot(
-    is(dane, 'okienko_df')
+    is(dane, 'baza_df')
   )
 
   dane = dane %>%
-    filter_(~ okres >= okres_min & okres <= okres_max) %>%
+    filter_(~ okres >= data_zak) %>%
     left_join(utrataEtatu) %>%
     mutate_(
-      roznica = ~ okres - data_zak,
-      utrpracy = ~ ifelse(is.na(utrpracy), FALSE, utrpracy)
+      roznica   = ~ okres - data_zak,
+      nutrpracy = ~ ifelse(is.na(utrpracy) | okres != data_zak, TRUE, !utrpracy)
     )
   if(multidplyr){
     dane = multidplyr::partition(dane, id_zdau)
@@ -24,32 +24,26 @@ oblicz_zmienne_czasowe = function(dane, utrataEtatu, multidplyr = TRUE){
   }
   dane = dane %>%
     summarize_(
-      czasmun     = ~ max(0, min(ifelse(mundur > 0, roznica, NA), na.rm = TRUE)),
-      czaspraw    = ~ max(0, min(ifelse(prawnik > 0, roznica, NA), na.rm = TRUE)),
-      czasprd     = ~ max(0, min(ifelse(etat + netat + samoz > 0, roznica, NA), na.rm = TRUE)),
-      czasprsd    = ~ max(0, min(ifelse(samoz > 0, roznica, NA), na.rm = TRUE)),
-      czasprud    = ~ max(0, min(ifelse(etat > 0, roznica, NA), na.rm = TRUE)),
-      czaszat     = ~ max(0, min(ifelse(etat + netat > 0, roznica, NA), na.rm = TRUE)),
-      czasmun_v2  = ~ max(0, min(ifelse(!utrpracy & mundur > 0, roznica, NA), na.rm = TRUE)),
-      czaspraw_v2 = ~ max(0, min(ifelse(!utrpracy & prawnik > 0, roznica, NA), na.rm = TRUE)),
-      czasprd_v2  = ~ max(0, min(ifelse(!utrpracy & etat + netat + samoz > 0, roznica, NA), na.rm = TRUE)),
-      czasprsd_v2 = ~ max(0, min(ifelse(!utrpracy & samoz > 0, roznica, NA), na.rm = TRUE)),
-      czasprud_v2 = ~ max(0, min(ifelse(!utrpracy & etat > 0, roznica, NA), na.rm = TRUE)),
-      czaszat_v2  = ~ max(0, min(ifelse(!utrpracy & etat + netat > 0, roznica, NA), na.rm = TRUE))
+      czasmun_v2  = ~ min(ifelse(nutrpracy & mundur > 0, roznica, NA_integer_), na.rm = TRUE),
+      czaspraw_v2 = ~ min(ifelse(nutrpracy & prawnik > 0, roznica, NA_integer_), na.rm = TRUE),
+      czasprd_v2  = ~ min(ifelse(nutrpracy & etat + netat + samoz > 0, roznica, NA_integer_), na.rm = TRUE),
+      czasprsd_v2 = ~ min(ifelse(nutrpracy & samoz > 0, roznica, NA_integer_), na.rm = TRUE),
+      czasprud_v2 = ~ min(ifelse(nutrpracy & etat > 0, roznica, NA_integer_), na.rm = TRUE),
+      czaszat_v2  = ~ min(ifelse(nutrpracy & etat + netat > 0, roznica, NA_integer_), na.rm = TRUE)
     ) %>%
     mutate_(
-      czasmun     = ~ ifelse(is.infinite(czasmun),  NA, czasmun),
-      czaspraw    = ~ ifelse(is.infinite(czaspraw), NA, czaspraw),
-      czasprd     = ~ ifelse(is.infinite(czasprd),  NA, czasprd),
-      czasprsd    = ~ ifelse(is.infinite(czasprsd), NA, czasprsd),
-      czasprud    = ~ ifelse(is.infinite(czasprud), NA, czasprud),
-      czaszat     = ~ ifelse(is.infinite(czaszat),  NA, czaszat),
-      czasmun_v2  = ~ ifelse(is.infinite(czasmun_v2),  NA, czasmun_v2),
-      czaspraw_v2 = ~ ifelse(is.infinite(czaspraw_v2), NA, czaspraw_v2),
-      czasprd_v2  = ~ ifelse(is.infinite(czasprd_v2),  NA, czasprd_v2),
-      czasprsd_v2 = ~ ifelse(is.infinite(czasprsd_v2), NA, czasprsd_v2),
-      czasprud_v2 = ~ ifelse(is.infinite(czasprud_v2), NA, czasprud_v2),
-      czaszat_v2  = ~ ifelse(is.infinite(czaszat_v2),  NA, czaszat_v2)
+      czasmun_v2  = ~ ifelse(is.infinite(czasmun_v2),  NA_integer_, czasmun_v2),
+      czaspraw_v2 = ~ ifelse(is.infinite(czaspraw_v2), NA_integer_, czaspraw_v2),
+      czasprd_v2  = ~ ifelse(is.infinite(czasprd_v2),  NA_integer_, czasprd_v2),
+      czasprsd_v2 = ~ ifelse(is.infinite(czasprsd_v2), NA_integer_, czasprsd_v2),
+      czasprud_v2 = ~ ifelse(is.infinite(czasprud_v2), NA_integer_, czasprud_v2),
+      czaszat_v2  = ~ ifelse(is.infinite(czaszat_v2),  NA_integer_, czaszat_v2),
+      czasmun     = ~ max(0, czasmun_v2),
+      czaspraw    = ~ max(0, czaspraw_v2),
+      czasprd     = ~ max(0, czasprd_v2),
+      czasprsd    = ~ max(0, czasprsd_v2),
+      czasprud    = ~ max(0, czasprud_v2),
+      czaszat     = ~ max(0, czaszat_v2)
     ) %>%
     collect()
   class(dane) = c('absolwent_df', class(dane))
