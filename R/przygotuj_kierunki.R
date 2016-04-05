778df93b59f2cc67cc9b83d38dafa082ce2afa30
Mateusz Żółtak <zozlak@zozlak.org>
2016-04-05 19:09:03 +0200

    Dodane dołączanie kodów i nazw obszarów, dziedzin i dyscyplin studiów

--- R/przygotuj_kierunki.R
+++ R/przygotuj_kierunki.R
@@ -11,5 +11,32 @@ przygotuj_kierunki = function(){
       jednostka    = 'jednostka_prowadzaca'
     ) %>%
     select_('uczelnia_id', 'jednostka_id', 'kierunek_id', 'kierunek')
-  return(dane)
+
+  obszary = read.csv2('dane/sl_kierunki_obszary.csv', stringsAsFactors = FALSE)
+  colnames(obszary) = tolower(colnames(obszary))
+  obszary = obszary %>%
+    rename_(
+      kierunek_id = 'kierunek'
+    ) %>%
+    group_by_('kierunek_id') %>%
+    summarize_(
+      obsz_kod      = ~ first(obszar_kod),
+      obsz          = ~ first(obszar),
+      dziedzin_kod  = ~ first(dziedzina_kod),
+      dziedzin      = ~ first(dziedzina),
+      dyscyp_kod    = ~ first(dyscyplina_kod),
+      dyscyp        = ~ first(dyscyplina),
+      n = ~ n()
+    ) %>%
+    mutate_(
+      obsz_kod      = ~ ifelse(n > 1, 99, obsz_kod),
+      obsz          = ~ ifelse(n > 1, 'studia międzyobszarowe', obsz),
+      dziedzin_kod  = ~ ifelse(n > 1, 99, dziedzin_kod),
+      dziedzin      = ~ ifelse(n > 1, 'studia międzydziedzinowe', dziedzin),
+      dyscyp_kod    = ~ ifelse(n > 1, 999, dyscyp_kod),
+      dyscyp        = ~ ifelse(n > 1, 'studia interdyscyplinarne', dyscyp)
+    ) %>%
+    select_('-n')
+
+  return(left_join(dane, obszary))
 }
