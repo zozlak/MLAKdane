14f8533c2400b3b6f989051cb8f7ecc8b592d3ae
Mateusz Żółtak <zozlak@zozlak.org>
2017-04-20 23:28:50 +0200

    zmiany w sposobie przygotowania danych o kierunkach (specyficzne dla zrzutu danych z 2017 roku)

--- R/przygotuj_kierunki.R
+++ R/przygotuj_kierunki.R
@@ -25,13 +25,19 @@ przygotuj_kierunki = function(katZr, zmDodatkowe = character()){
       jednostka_id      = ~ifelse(length(unique(jednostka_id)) > 1, NA, first(jednostka_id)),
       jednostka         = ~ifelse(length(unique(jednostka_id)) > 1, 'studia międzywydziałowe', first(jednostka)),
       uczelnia_id       = ~ifelse(length(unique(uczelnia_id)) > 1, NA, first(uczelnia_id)),
-      uczelnia          = ~ifelse(length(unique(uczelnia_id)) > 1, 'studia między uczelniane', first(uczelnia))
+      uczelnia          = ~ifelse(length(unique(uczelnia_id)) > 1, 'studia między uczelniane', first(uczelnia)),
+      obsz_kod          = ~suppressWarnings(as.integer(sub('^[A-Z]+', '', first(wiodaca_obszar_kod)))),
+      obsz              = ~first(wiodaca_obszar),
+      dzie_kod          = ~suppressWarnings(as.integer(sub('^[A-Z]+', '', first(wiodaca_dziedz_kod)))),
+      dzie              = ~first(wiodaca_dziedz),
+      dysc_kod          = ~suppressWarnings(as.integer(sub('^[A-Z]+', '', first(wiodaca_dysc_kod)))),
+      dysc              = ~first(wiodaca_dysc)
     ) %>%
     mutate_(
       kieruneknazwa = ~paste0(jednostka, ', ', kierunek, ', ', slFormy[forma_ksztalcenia], ' (POLon: ', kierunek_id, ')')
     ) %>%
     select_(
-      .dots = c('uczelnia_id', 'jednostka_id', 'kierunek_id', 'kieruneknazwa', zmDodatkowe)
+      .dots = c('uczelnia_id', 'jednostka_id', 'kierunek_id', 'kieruneknazwa', 'obsz_kod', 'obsz', 'dzie_kod', 'dzie', 'dysc_kod', 'dysc', zmDodatkowe)
     )
 
   obszary = utils::read.csv2(paste0('dane/sl_kierunki_obszary.csv'), stringsAsFactors = FALSE)
@@ -44,26 +50,35 @@ przygotuj_kierunki = function(katZr, zmDodatkowe = character()){
     ) %>%
     group_by_('kierunek_id') %>%
     summarize_(
-      obsz_kod  = ~ first(obsz_kod),
-      obsz      = ~ first(obsz),
-      dzie_kod  = ~ first(dzie_kod),
-      dzie      = ~ first(dzie),
-      dysc_kod  = ~ first(dysc_kod),
-      dysc      = ~ first(dysc),
+      obsz_kod2  = ~ first(obsz_kod),
+      obsz2      = ~ first(obsz),
+      dzie_kod2  = ~ first(dzie_kod),
+      dzie2      = ~ first(dzie),
+      dysc_kod2  = ~ first(dysc_kod),
+      dysc2      = ~ first(dysc),
       n = ~ n()
     ) %>%
     mutate_(
-      obsz_kod  = ~ ifelse(n > 1, 99, obsz_kod),
-      obsz      = ~ ifelse(n > 1, 'studia międzyobszarowe', obsz),
-      dzie_kod  = ~ ifelse(n > 1, 99, dzie_kod),
-      dzie      = ~ ifelse(n > 1, 'studia międzydziedzinowe', dzie),
-      dysc_kod  = ~ ifelse(n > 1, 999, dysc_kod),
-      dysc      = ~ ifelse(n > 1, 'studia interdyscyplinarne', dysc)
+      obsz_kod2  = ~ ifelse(n > 1, 99, obsz_kod2),
+      obsz2      = ~ ifelse(n > 1, 'studia międzyobszarowe', obsz2),
+      dzie_kod2  = ~ ifelse(n > 1, 99, dzie_kod2),
+      dzie2      = ~ ifelse(n > 1, 'studia międzydziedzinowe', dzie2),
+      dysc_kod2  = ~ ifelse(n > 1, 999, dysc_kod2),
+      dysc2      = ~ ifelse(n > 1, 'studia interdyscyplinarne', dysc2)
     ) %>%
     select_('-n')
-
   dane = dane %>%
-    left_join(obszary)
+    left_join(obszary) %>%
+    mutate_(
+      obsz_kod  = ~ dplyr::coalesce(obsz_kod, obsz_kod2),
+      obsz      = ~ dplyr::coalesce(obsz, obsz2),
+      dzie_kod  = ~ dplyr::coalesce(dzie_kod, dzie_kod2),
+      dzie      = ~ dplyr::coalesce(dzie, dzie2),
+      dysc_kod  = ~ dplyr::coalesce(dysc_kod, dysc_kod2),
+      dysc      = ~ dplyr::coalesce(dysc, dysc2)
+    ) %>%
+    select_('-obsz_kod2', '-obsz2', '-dzie_kod2', '-dzie2', '-dysc_kod2', '-dysc2')
+
   if (any(is.na(dane$obsz_kod))) {
     warning('brak informacji o obszarach/dziedzinach/dyscyplinach dla ', sum(is.na(dane$obsz_kod)), ' kierunków')
   }
